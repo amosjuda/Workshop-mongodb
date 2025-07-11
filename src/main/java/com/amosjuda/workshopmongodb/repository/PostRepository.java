1774784af16c63b275a599e6d8b04cb2d93b48ad
amosjuda <amosjudasilva@gmail.com>
2025-07-11 10:27:56 -0300

    feat: simple query with @Query

--- src/main/java/com/amosjuda/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/amosjuda/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.amosjuda.workshopmongodb.repository;
 import com.amosjuda.workshopmongodb.domain.Post;
 import com.amosjuda.workshopmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
